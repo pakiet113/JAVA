9a1732f23a7617d5900c0e9c28d86c28a7454bd4
Anh Kiet <37829865+pakiet113@users.noreply.github.com>
2018-04-15 19:48:19 +0700

    BAITH2

---
+++ BAITH2/src/baith2/DANHSACH.java
@@ -0,0 +1,42 @@
+package baith2;
+
+/**
+ *
+ * @author bang
+ */
+public class DANHSACH {
+    private int STT;
+    private String name;
+    private String gioiTinh;
+    private String namSinh;
+    public void setSTT(int stt){
+        this.STT = stt;
+    }
+    public int getSTT(){
+        return STT;
+    }
+    public void setGioiTinh(String gioiTinh){
+        this.gioiTinh = gioiTinh;
+    }
+    public String getGioiTinh(){
+        return gioiTinh;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setNamSinh(String namSinh){
+        this.namSinh = namSinh;
+    }
+    public String getNamSinh(){
+        return namSinh;
+    }
+
+    /*void getName(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }*/
+    
+    
+}
